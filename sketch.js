fa536ecd612ddf605b81f4462d6c5c859f8b697d
RuanGABRIELba <137806794+RuanGABRIELba@users.noreply.github.com>
2023-07-03 12:02:17 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(600, 600);
+   background("#2885E5");
+}
+  
+function draw(){
+ fill ("red");
+if(mouseIsPressed)
+rect(mouseX,mouseY,25,25);
+  stroke("blue")
+}
